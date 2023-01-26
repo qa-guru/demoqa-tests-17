f8d7bf98dc372af863f3ceab623c68f3c1c3d4d0
svasenkov <aanher@gmail.com>
2023-01-26 22:02:50 +0300

    added remote test

---
+++ src/test/java/demoqa/tests/TestBaseExtended.java
@@ -0,0 +1,28 @@
+package demoqa.tests;
+
+import com.codeborne.selenide.Configuration;
+import demoqa.pages.RegistrationPage;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+public class TestBaseExtended {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "100.0";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
+    }
+}
